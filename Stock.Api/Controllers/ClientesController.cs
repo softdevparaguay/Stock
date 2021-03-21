a6a94344a9ba6547f0b030f7b9f45752b343e4de
Leonardo Testa <loscabres@hotmail.com>
2021-03-21 09:57:50 -0300

    Controlador de la clase clientes

---
+++ Stock.Api/Controllers/ClientesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClientesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Obtener()
+        {
+            //return this. NotFound("NO se encontró ningun registro");
+            //return BadRequest("Ha Ocurrido Un Error");
+            
+            return Ok(new { Id_Clientes = 1, PrimerNombre = "Leonardo" });
+        }
+    }
+}
